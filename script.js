2a884c99b0eee211e2d281ec8ac7a97be557d950
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-04-09 16:05:45 -0700

    updated formulas with calculations for pv, int, and time

--- script.js
+++ script.js
@@ -22,21 +22,22 @@ $(document).ready(function () {
     }
   };
 
-   // future value calculation is FV = PV(1 + i)^time
-  /* ******* for first exercise slide in lecture ********** */
+  // future value calculation is FV = PV(1 + i)^time
 
-  
 
-  $(document).on('click', '.calculate-btn', calc_FV)
-  
+  $(document).on('click', '.futurevalue', calc_FV);
+  $(document).on('click', '.presentvalue', calc_PV);
+  $(document).on('click', '.interest', calc_i);
+  $(document).on('click', '.time', calc_n);
+
+
+  /* future value calculation ******************* */
   function calc_FV() {
     let PV = parseFloat(document.getElementById('PV').value);
     let i = parseFloat(document.getElementById('i').value) / 100;
     // console.log(i)
     let n = parseInt(document.getElementById('n').value);
     let ans = document.getElementById('answer');
-
-  
 
     if (isNaN(PV) || PV <= 0) {
       ans.innerHTML = `Investment must be a valid number greater than zero.`;
@@ -54,6 +55,91 @@ $(document).ready(function () {
     }
   }
   /* ****************** end future value calculation ******************* */
+
+
+
+  // present value calculation ********************* 
+  function calc_PV() {
+    let FV = parseFloat(document.getElementById('form1fv').value);
+    let i = parseFloat(document.getElementById('form1i').value) / 100;
+    // console.log(i)
+    let n = parseInt(document.getElementById('form1n').value);
+    let ans = document.getElementById('form1-ans');
+
+    if (isNaN(FV) || FV <= 0) {
+      ans.innerHTML = `Future value must be a valid number greater than zero.`;
+    }
+    else if (isNaN(i) || i <= 0) {
+      ans.innerHTML = `Annual rate must be a valid number greater than zero.`;
+    }
+    else if (isNaN(n) || n <= 0) {
+      ans.innerHTML = `Years must be a valid number greater than zero.`;
+    }
+    else {
+      let PV = FV * (1 / (Math.pow((1 + i), n)));
+      PV = PV.toFixed(2);
+      ans.innerHTML = `The interest rate (i) is $${i}`;
+    }
+  }
+  /* ****************** end present value calculation ******************* */
+
+
+
+
+  // interest calculation ********************* 
+  function calc_i() {
+    let FV = parseFloat(document.getElementById('form2fv').value);
+    let PV = parseFloat(document.getElementById('form2pv').value);
+    let n = parseInt(document.getElementById('form2n').value);
+    let ans = document.getElementById('form2-ans');
+
+    if (isNaN(FV) || FV <= 0) {
+      ans.innerHTML = `Future value must be a valid number greater than zero.`;
+    }
+    else if (isNaN(PV) || PV <= 0) {
+      ans.innerHTML = `Present Value must be a valid number greater than zero.`;
+    }
+    else if (isNaN(n) || n <= 0) {
+      ans.innerHTML = `Years must be a valid number greater than zero.`;
+    }
+    else {
+      let i = (Math.pow((FV / PV), 1 / n)) - 1;
+      i = i.toFixed(3);
+      ans.innerHTML = `The interest rate (i) is ${i}%`;
+    }
+  }
+  /* ****************** end interest calculation ******************* */
+
+
+
+
+  // time calculation ********************* 
+  function calc_n() {
+    let FV = parseFloat(document.getElementById('form3fv').value);
+    let PV = parseFloat(document.getElementById('form3pv').value);
+    let i = parseFloat(document.getElementById('form3i').value) / 100;
+    let n;
+    let ans = document.getElementById('form3-ans');
+
+    if (isNaN(FV) || FV <= 0) {
+      ans.innerHTML = `Future value must be a valid number greater than zero.`;
+    }
+    else if (isNaN(PV) || PV <= 0) {
+      ans.innerHTML = `Present Value must be a valid number greater than zero.`;
+    }
+    else if (isNaN(i) || i <= 0) {
+      ans.innerHTML = `Annual rate must be a valid number greater than zero.`;
+    }
+    else {
+      let y = (Math.log(FV));
+      let x = (Math.log((1 + i)));
+      n = y / x;
+      n = n.toFixed(2);
+
+      ans.innerHTML = `The time (n) required for the investment to double is ${n} years.`;
+    }
+  }
+  /* ****************** end time calculation ******************* */
 
 
 
